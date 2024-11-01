5a7790e2934969666a3ed0068d26532c14d16ebb
Zuellni <123005779+Zuellni@users.noreply.github.com>
2024-11-01 23:23:14 +0100

    Some tweaks

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ class Model {
 		this.model = tf.sequential()
 		this.model.add(tf.layers.batchNormalization({ inputShape: [shape] }))
 		this.model.add(tf.layers.dense({ units: 256, activation: "relu", kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }) }))
-		this.model.add(tf.layers.dropout({ rate: 0.3 }))
+		this.model.add(tf.layers.dropout({ rate: 0.2 }))
 		this.model.add(tf.layers.dense({ units: 64, activation: "relu", kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }) }))
-		this.model.add(tf.layers.dropout({ rate: 0.3 }))
+		this.model.add(tf.layers.dropout({ rate: 0.2 }))
 		this.model.add(tf.layers.dense({ units: 1, activation: "sigmoid" }))
 		this.model.compile({ loss: "meanSquaredError", metrics: "mse", optimizer: "adam" })
 	}
@@ -202,11 +202,7 @@ const prepareData = (entries, database) => {
 				input[index] = 1
 		}
 
-		const tagCount = input.reduce((sum, val) => sum + val, 0)
-
-		if (tagCount > 0) {
-			// input = input.map(val => val / tagCount)
-
+		if (input.some(value => value == 1)) {
 			if (score)
 				dataTrain.push({
 					input: input,
